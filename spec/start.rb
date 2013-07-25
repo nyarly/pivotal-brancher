16a251b4d7348428365bc341ff3e73062f00813b
Judson <nyarly@gmail.com>
2013-07-25 16:17:30 -0700

    Coverage is very touchy on small projects

--- spec/start.rb
+++ spec/start.rb
@@ -15,32 +15,51 @@ describe PivotalBrancher::Cli do
     end
   end
 
-  let :brancher_app do
-    PivotalBrancher::App.new.tap do |app|
-      app.stub(:started_stories).and_return(stories)
+  describe "with started stories" do
+    let :brancher_app do
+      PivotalBrancher::App.new.tap do |app|
+        app.stub(:started_stories).and_return(stories)
+      end
+    end
+
+    let :stories do
+      [
+        OpenStruct.new(:id => "123456", :name => "This: is a story")
+      ]
+    end
+
+    it "should report on stories considered" do
+      expect(capture_stdout do
+        cli.start
+      end).to match(/is a story/)
+    end
+
+    it "should build branch names" do
+      expect(cli.story_branch_name(stories.first)).to eql("123456_this_is_a_story")
+    end
+
+    it "should execute git checkout -b" do
+      cli.should_receive(:run).with("git checkout -b 123456_this_is_a_story")
+      capture_stdout do
+        cli.start
+      end
     end
   end
 
-  let :stories do
-    [
-      OpenStruct.new(:id => "123456", :name => "This: is a story")
-    ]
-  end
+  describe "with no started stories" do
+    let :brancher_app do
+      PivotalBrancher::App.new.tap do |app|
+        app.stub(:started_stories).and_return([])
+      end
+    end
 
-  it "should report on stories considered" do
-    expect(capture_stdout do
-      cli.start
-    end).to match(/is a story/)
-  end
-
-  it "should build branch names" do
-    expect(cli.story_branch_name(stories.first)).to eql("123456_this_is_a_story")
-  end
-
-  it "should execute git checkout -b" do
-    cli.should_receive(:run).with("git checkout -b 123456_this_is_a_story")
-    capture_stdout do
-      cli.start
+    it "should not execute git" do
+      cli.should_not_receive(:run)
+      capture_stdout do
+        expect do
+          cli.start
+        end.to raise_error(SystemExit)
+      end
     end
   end
 end
